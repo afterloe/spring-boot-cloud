fa15977e681befa139dd20770022fbad179ab24d
afterloe <lm6289511@gmail.com>
2017-04-18 15:55:01 +0800

    添加服务发现demo

--- netflix/src/main/java/com/github/afterloe/LanchApplication.java
+++ netflix/src/main/java/com/github/afterloe/LanchApplication.java
@@ -11,16 +11,20 @@
  */
 package com.github.afterloe;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configurable
+@Configuration
+@ComponentScan
 @EnableAutoConfiguration
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class LanchApplication {
 
