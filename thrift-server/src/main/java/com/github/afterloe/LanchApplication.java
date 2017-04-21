91caea5663f6d7f7e057b2250ba855ca35e602fa
afterloe <lm6289511@gmail.com>
2017-04-21 15:52:15 +0800

    添加系统显示资源

--- thrift-server/src/main/java/com/github/afterloe/LanchApplication.java
+++ thrift-server/src/main/java/com/github/afterloe/LanchApplication.java
@@ -12,7 +12,13 @@
 package com.github.afterloe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
 public class LanchApplication {
 
 	public static void main(String[] args) {
